da7c412a07c4b4535496032380fe37373c007a15
tKnmori <knmori.takashi@gmail.com>
2023-03-11 01:11:51 +0900

    Add preprocessor guards in case CSS_COMPOSITION is not defined.

--- Source/WebCore/rendering/RenderLayerBacking.cpp
+++ Source/WebCore/rendering/RenderLayerBacking.cpp
@@ -1117,14 +1117,14 @@ bool RenderLayerBacking::updateConfiguration(const RenderLayer* compositingAnces
         m_graphicsLayer->setAppliesDeviceScale(!unscaledBitmap);
         layerConfigChanged = true;
     }
-
+#if ENABLE(CSS_COMPOSITING)
     bool shouldPaintUsingCompositeCopy = unscaledBitmap && is<RenderHTMLCanvas>(renderer());
     if (shouldPaintUsingCompositeCopy != m_owningLayer.shouldPaintUsingCompositeCopy()) {
         m_owningLayer.setShouldPaintUsingCompositeCopy(shouldPaintUsingCompositeCopy);
         m_graphicsLayer->setShouldPaintUsingCompositeCopy(shouldPaintUsingCompositeCopy);
         layerConfigChanged = true;
     }
-
+#endif
     if (is<RenderEmbeddedObject>(renderer()) && downcast<RenderEmbeddedObject>(renderer()).allowsAcceleratedCompositing()) {
         auto* pluginViewBase = downcast<PluginViewBase>(downcast<RenderWidget>(renderer()).widget());
 #if PLATFORM(IOS_FAMILY)
